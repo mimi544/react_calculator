8669f8f6c39d85a919fb5a6970aad94e09592500
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-01-31 22:11:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import moonIcon from "./assets/moon.png";
+import sunIcon from "./assets/sun.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const[darkmode, setDarkmode] = React.useState(false);
+	return (
+		<div className="App">
+			<div className="smart_calculator">
+				<div className="smart_calculator_navbar">
+					<div className="smart_calculator_navbar_toogle">
+						<div className="smart_calculator_navbar_round" />
+					</div>
+          <img src={} alt = "mode"/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
