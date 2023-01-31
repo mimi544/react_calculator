8c69ff4b1146dd0b508ed06efa2074021df77031
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-02-01 00:11:38 +0530

    styling header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,16 @@ import "./Header.css";
 function Header() {
 	return (
 		<div className="header">
-			<h1>Header</h1>
+			<div className="calculator_history">
+				<p>10+2</p>
+				<p>125-878*4</p>
+				<p>10+5</p>
+			</div>
+			<br />
+			<div className="mathematical_expression">
+				<p>10+50+89</p>
+			</div>
+			<p className="calculator_result">149</p>
 		</div>
 	);
 }
