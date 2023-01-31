9f21b484f245c4b9e06fa26c58df449d5bd6e6ab
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-01-31 23:40:23 +0530

    toggle working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { useState } from "react";
+
 import "./App.css";
 import moonIcon from "./assets/moon.png";
 import sunIcon from "./assets/sun.png";
+import Header from "./Components/Header/Header";
+import NumPad from "./Components/NumPad/NumPad";
 
 function App() {
-	const [darkmode, setDarkmode] = React.useState(false);
+	const [darkmode, setdarkmode] = useState(false);
 	return (
-		<div className="App">
+		<div className="App" data-theme={darkmode ? "dark" : ""}>
 			<div className="smart_calculator">
 				<div className="smart_calculator_navbar">
-					<div className="smart_calculator_navbar_toggle">
-						<div className="smart_calculator_navbar_toggle_round" />
+					<div
+						className="smart_calculator_navbar_toggle"
+						onClick={() => setdarkmode(!darkmode)}
+					>
+						<div
+							className={`smart_calculator_navbar_toggle_round ${
+								darkmode
+									? "smart_calculator_navbar_toggle_round_active"
+									: ""
+							}`}
+						/>
 					</div>
-					{/* <img src={} alt = "mode"/> */}
+					<img src={darkmode ? moonIcon : sunIcon} alt="mode" />
 				</div>
+
+				<Header />
+				<NumPad />
 			</div>
 		</div>
 	);
---
+++ src/Components/NumPad/NumPad.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NumPad.css";
+const NumPad = () => {
+	return (
+		<div className="numpad">
+			<h1>Num Pad</h1>
+		</div>
+	);
+};
+
+export default NumPad;
