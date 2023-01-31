2e615348e2458776150cf92d8ff0135706a5b754
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-01-31 22:20:57 +0530

    toggle correction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import moonIcon from "./assets/moon.png";
-import sunIcon from "./assets/sun.png";
+// import moonIcon from "./assets/moon.png";
+// import sunIcon from "./assets/sun.png";
 
 function App() {
-  const[darkmode, setDarkmode] = React.useState(false);
+	const [darkmode, setDarkmode] = React.useState(false);
 	return (
 		<div className="App">
 			<div className="smart_calculator">
 				<div className="smart_calculator_navbar">
-					<div className="smart_calculator_navbar_toogle">
-						<div className="smart_calculator_navbar_round" />
+					<div className="smart_calculator_navbar_toggle">
+						<div className="smart_calculator_navbar_toggle_round" />
 					</div>
-          <img src={} alt = "mode"/>
+					{/* <img src={} alt = "mode"/> */}
 				</div>
 			</div>
 		</div>
