fc4c7c02863a3e6774d97e32073dfa0c9ce3065f
mimi544 <59132699+mimi544@users.noreply.github.com>
2023-02-01 08:46:54 +0530

    working on keys

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./Header.css";
 function Header() {
 	return (
-		<div className="header">
+		<div className="header custom-scroll">
 			<div className="calculator_history">
+				<p>10+2</p>
+				<p>125-878*4</p>
+				<p>10+5</p>
 				<p>10+2</p>
 				<p>125-878*4</p>
 				<p>10+5</p>
 			</div>
 			<br />
-			<div className="mathematical_expression">
+			<div className="mathematical_expression custom-scroll">
 				<p>10+50+89</p>
 			</div>
 			<p className="calculator_result">149</p>
--- src/Components/NumPad/NumPad.js
+++ src/Components/NumPad/NumPad.js
@@ -1,9 +1,102 @@
 import React from "react";
 import "./NumPad.css";
 const NumPad = () => {
+	const nums = [
+		{
+			keyCode: 55,
+			label: "7",
+		},
+		{
+			keyCode: 56,
+			label: "8",
+		},
+		{
+			keyCode: 57,
+			label: "9",
+		},
+		{
+			keyCode: 52,
+			label: "4",
+		},
+		{
+			keyCode: 53,
+			label: "5",
+		},
+		{
+			keyCode: 54,
+			label: "6",
+		},
+		{
+			keyCode: 49,
+			label: "1",
+		},
+		{
+			keyCode: 50,
+			label: "2",
+		},
+		{
+			keyCode: 51,
+			label: "3",
+		},
+		{
+			keyCode: 48,
+			label: "0",
+		},
+		{
+			keyCode: 190,
+			label: ".",
+		},
+		{
+			keyCode: 13,
+			label: "=",
+		},
+	];
+
+	const signs = [
+		{
+			label: "⌫",
+			keyCode: 8,
+			value: "backspace",
+		},
+		{
+			label: "÷",
+			keyCode: 111,
+			value: "/",
+		},
+		{
+			label: "×",
+			keyCode: 56,
+			value: "*",
+		},
+		{
+			label: "﹣",
+			keyCode: 109,
+			value: "-",
+		},
+		{
+			label: "+",
+			keyCode: 107,
+			value: "+",
+		},
+		{
+			label: "%",
+			keyCode: 53,
+			value: "%",
+		},
+	];
+
 	return (
 		<div className="numpad">
-			<h1>Num Pad</h1>
+			<div className="numpad_nums">
+				{nums.map((item, index) => (
+					<p key={index}>{item.label}</p>
+				))}
+			</div>
+			<div className="numpad_signs">
+				{signs.map((item, index) => (
+					<p key={index}>{item.label}</p>
+				))}
+			</div>
 		</div>
 	);
 };
